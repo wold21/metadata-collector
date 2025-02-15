756e4b027726a516dc441368bded65eff1985bbe
한승재 <hanseungjae@hanseungjaeui-MacBookPro.local>
2025-02-16 02:04:56 +0900

    Refactor: Request Full URL 출력 추가 (정보 확인용)

--- utils/common_request.py
+++ utils/common_request.py
@@ -1,6 +1,10 @@
 import requests
 
 def get(url, params):
+    # 요청 URL 생성 및 출력
+    full_url = f"{url}?" + "&".join([f"{key}={value}" for key, value in params.items()])
+    print(f"✅ Request URL: {full_url}")
+
     response = requests.get(url, params=params)
     if(response.status_code == 200):
         return response.json()
