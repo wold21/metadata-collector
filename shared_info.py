f3f91ae83f6ade25d5494c8d6be7e3c1ee7c0fa1
hanseungjae <gkstmdwo100@naver.com>
2025-04-02 01:01:53 +0900

    feat: musicbrainz 기본 User-Agent 설정정보 추가

--- shared_info.py
+++ shared_info.py
@@ -2,6 +2,8 @@ class SharedInfo:
     api_key = None
     lastfm_base_url = None
     musicbrainz_base_url = None
+    user_agent = "YourAppName/1.0 (gkstmdwo100@gmail.com)"  # 기본 User-Agent 설정
+
 
     @classmethod
     def get_api_key(cls):
@@ -24,6 +26,13 @@ class SharedInfo:
         return cls.musicbrainz_base_url
     
     @classmethod
+    def get_user_agent(cls):
+        return cls.user_agent
+    
+    @classmethod
     def set_musicbrainz_base_url(cls, key):
         cls.musicbrainz_base_url = key
-    
+
+    @classmethod
+    def set_user_agent(cls, agent):
+        cls.user_agent = agent
--- utils/common_request.py
+++ utils/common_request.py
@@ -1,5 +1,6 @@
 import requests
 from utils.logging_config import logger
+from shared_info import SharedInfo
 
 
 def get(url, params):
@@ -7,7 +8,9 @@ def get(url, params):
     full_url = f"{url}?" + "&".join([f"{key}={value}" for key, value in params.items()])
     logger.info(f"✅ Request URL: {full_url}")
 
-    response = requests.get(url, params=params)
+    headers = {"User-Agent": SharedInfo.get_user_agent()} 
+    response = requests.get(url, params=params, headers=headers) 
+
     if(response.status_code == 200):
         return response.json()
     else:
